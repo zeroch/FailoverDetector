c8cc3736d6ff4c7ef85764593ef5af25ab42626e
Ze Chen <zeche@microsoft.com>
2017-05-08 17:08:27 -0700

    demo create a table into database

--- FailoverDetector/Program.cs
+++ FailoverDetector/Program.cs
@@ -11,20 +11,34 @@ namespace FailoverDetector
     {
         static void Main(string[] args)
         {
-            TestingSQLCommand testCommand = new TestingSQLCommand();
+
+            string connStr = "Server=ze-vm001\\SQL18TEST01; Initial Catalog=FailoverDetector; Trusted_Connection=True;";
+            TestingSQLCommand testCommand = new TestingSQLCommand(connStr);
             testCommand.TestSQLConnection();
+            testCommand.TestSQLPrepare("NodeA");
+            Console.ReadLine(); 
         }
     }
 
-    class TestingSQLCommand
+    public class TestingSQLCommand
     {
+        private String connString;
+
+        public TestingSQLCommand(string connStr)
+        {
+            connString = connStr;
+        }
+        public String getConnString()
+        {
+            return connString;
+        }
+
         public void TestSQLConnection()
         {
-            string connString = "Server=ze-vm001\\SQL18TEST01; Initial Catalog=FailoverDetector; Trusted_Connection=True;";
             string queryString = "select @@servername";
             try
             {
-                using (SqlConnection conn = new SqlConnection(connString))
+                using (SqlConnection conn = new SqlConnection(this.connString))
                 {
                     SqlCommand command = new SqlCommand(queryString, conn);
                     command.Connection.Open();
@@ -37,5 +51,43 @@ namespace FailoverDetector
             }
             
         }
+
+        public void TestSQLPrepare(string instanceName)
+        {
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(this.connString))
+                {
+                    conn.Open();
+                    SqlCommand command = new SqlCommand(null, conn);
+
+                    command.CommandText =
+                        "CREATE TABLE " + instanceName + " (" +
+                        " [server_name] [NVARCHAR](128) NULL," +
+                        " [event_name][NVARCHAR](60) NOT NULL," +
+                        " [timeStamp][datetimeoffset](7)," +
+                        "[current_state][NVARCHAR](60) NULL," +
+                        "[message][NVARCHAR](250) NULL" +
+                        ")";
+
+
+                    //SqlParameter dbParam = new SqlParameter("@NodeName", System.Data.SqlDbType.Text, 20);
+                    //dbParam.Value = instanceName;
+                    //command.Parameters.Add(dbParam);
+                    command.Prepare();
+                    var ret = command.ExecuteScalar();
+                    if (ret != null)
+                        Console.WriteLine(ret.ToString());
+                    else
+                        Console.WriteLine("No return value from sql execution.");
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
     }
 }
