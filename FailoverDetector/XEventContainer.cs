e1f3481c10a9d96d724d9ef2967b005fd16fec65
Ze Chen <zeche@microsoft.com>
2017-05-12 12:47:57 -0700

    XEvent container structural

---
+++ FailoverDetector/XEventContainer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Microsoft.SqlServer.XEvent.Linq;
+
+namespace FailoverDetector
+{
+    class XEventContainer
+    {
+        enum XEventType
+        {
+            AlwaysOnHealth,
+            SystemHealth,
+        };
+
+        private string instanceName;
+        private const string connectionString = "Initial Catalog=FailoverDetector; Trusted_Connection=True;";
+        private SqlConnection m_conn;
+        private SqlCommand insert_command;
+        private QueryableXEventData raw_data;
+
+        private bool openXelFile(string fileName)
+        {
+            if (fileName == null)
+            {
+                return false;
+            }
+            else
+            {try
+                {
+                    raw_data = new QueryableXEventData(fileName);
+                    return true;
+                }catch(Exception e)
+                {
+                    Console.WriteLine(e);
+                    Console.WriteLine("open XelFiel failed");
+                    return false;
+                }
+            }
+        }
+
+        private void createTable()
+        {
+        }
+
+        private void insertDataToTable()
+        {
+
+        }
+        public void filterNoise()
+        {
+
+        }
+
+        
+
+    }
+   
+}
