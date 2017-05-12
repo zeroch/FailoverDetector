eae5c8c89adc7e3b177d8eb5814a69c8f73ad856
Ze Chen <zeche@microsoft.com>
2017-05-12 13:58:57 -0700

    add failover report base class

---
+++ FailoverDetector/FailoverReport.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FailoverDetector
+{
+    class FailoverReport
+    {
+        // property
+        private DateTimeOffset  failoverTime;
+        private string          currentPrimary;
+        private string previousPrimary;
+        private bool failoverResult;
+        private string rootCause;
+        private IList<XEventContainer> dataSourceList;
+
+        public void addDataSource(XEventContainer dataContainer)
+        {
+            dataSourceList.Add(dataContainer);
+        }
+        public virtual void buildReport()
+        {
+        }
+
+        public virtual void analyzeData()
+        {
+
+        }
+        public virtual void showReport()
+        {
+
+        }
+
+
+    }
+}
