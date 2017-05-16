761a930be216fd702e1feb886a94aa21493ab4ad
Ze Chen <zeche@microsoft.com>
2017-05-16 12:31:53 -0700

    base container

---
+++ FailoverDetector/DataContainer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FailoverDetector
+{
+    public class DataContainer
+    {
+        public DataContainer() { }
+        public DataContainer(string filename) { }
+        public DataContainer(string[] fileList) { }
+        public virtual void loadDataFile() { }
+        public virtual void filterData() { }
+        public virtual void DetectFilover() { }
+        public virtual void SearchFilover() { }
+        public virtual void SearchLeaseTimeout() { }
+        public virtual void SearchHealthCheckTimeout() { }
+        public virtual void SearchComponentResult() { }
+    }
+}
