2132d9eff8d53fe0e29bc44aa695828624989cee
Ze Chen <zeche@microsoft.com>
2017-05-16 13:48:56 -0700

    data object provides IEnumberable object to contain xevent rows

---
+++ FailoverDetector/DataObject.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.SqlServer.XEvent.Linq;
+
+namespace FailoverDetector
+{
+    public class AlwaysOnData
+    {
+        public EventList arStateChangeEvent;
+        public EventList arMgrStateChangeEvent;
+        public EventList errorEvent;
+
+        public AlwaysOnData()
+        {
+            arStateChangeEvent = new EventList();
+            arMgrStateChangeEvent = new EventList();
+            errorEvent = new EventList();
+        }
+    }
+    public class SystemData
+    {
+        public EventList spDiagResultEvents;
+        public SystemData()
+        {
+            spDiagResultEvents = new EventList();
+        }
+    }
+    public class EventList : IEnumerable
+    {
+        public List<PublishedEvent> events;
+
+        public EventList()
+        {
+            events = new List<PublishedEvent>();
+        }
+        public void append(PublishedEvent evt) { events.Add(evt); }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            yield return (IEnumerable)GetEnumerator();
+        }
+        public EventListEnum GetEnumerator()
+        {
+            return new EventListEnum(events);
+        }
+    }
+    public class EventListEnum : IEnumerator
+    {
+        public List<PublishedEvent> _events;
+        int pos = -1;
+
+        public EventListEnum(List<PublishedEvent> list)
+        {
+            _events = list;
+        }
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+        
+        public PublishedEvent Current
+        {
+            get
+            {
+                try
+                {
+                    return _events[pos];
+                }catch( IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+
+        public bool MoveNext()
+        {
+            pos++;
+            return (pos < _events.Count);
+        }
+
+        public void Reset()
+        {
+            pos = -1;
+        }
+    }
+}
