d929000ea58bed95daa86d32b5b955cf56553e24
Ze Chen <zeche@microsoft.com>
2017-06-27 16:34:03 -0700

    dispatch message

---
+++ FailoverDetector.Test/AlwaysOnDataTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FailoverDetector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FailoverDetector.Tests
+{
+    public class AlwaysOnDataTests
+    {
+        [TestMethod()]
+        public void AlwaysOnDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void loadDataTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
--- FailoverDetector/DataObject.cs
+++ FailoverDetector/DataObject.cs
@@ -17,11 +17,63 @@ namespace FailoverDetector
             arMgrStateChangeEvent = new EventList();
             errorEvent = new EventList();
         }
+        enum AlwaysOn_EventType {
+            DLL_EXECUTED,
+            AG_LEASE_EXPIRED,
+            AR_MANGER_STATE_CHANGE,
+            AR_STATE,
+            AR_STATE_CHANGE,
+            LOCK_REDO_BLOCKED,
+            ERROR
+        }
+        public void HandleDDLExecuted(PublishedEvent evt) { }
+        public void HandleAGLeaseExpired(PublishedEvent evt) { }
+        public void HandleARMgrStateChange(PublishedEvent evt) { }
+        public void HandleARState(PublishedEvent evt) { }
+        public void HandleARStateChange(PublishedEvent evt) { }
+        public void HandleLockRedoBlocked(PublishedEvent evt) { }
+        public void HandleErrorReported(PublishedEvent evt) { }
+        private void DispatchEvent(PublishedEvent evt)
+        {
+            switch(evt.Name)
+            {
+                case "alwayson_ddl_executed":
+                    HandleDDLExecuted(evt);
+                    break;
+                case "availability_group_lease_expired":
+                    HandleAGLeaseExpired(evt);
+                    break;
+                case "availability_replica_manager_state_change":
+                    HandleARMgrStateChange(evt);
+                    break;
+                case "availability_replica_state":
+                    HandleARState(evt);
+                    break;
+                case "availability_replica_state_change":
+                    HandleARStateChange(evt);
+                    break;
+                case "lock_redo_blocked":
+                    HandleLockRedoBlocked(evt);
+                    break;
+                case "error_reported":
+                    HandleErrorReported(evt);
+                    break;
+                default:
+                    break;
+            }
+        }
         public void loadData(string xelFileName, string serverName)
-        {  
+        {
             // load xel File
-            // filter data
-            // extract into alwaysOnData
+            using (QueryableXEventData events = new QueryableXEventData(xelFileName))
+            {
+                foreach(PublishedEvent evt in events)
+                {
+                    // dispatch event and handle by own method.
+                    DispatchEvent(evt);
+                }
+            }
+
            
         }
         
