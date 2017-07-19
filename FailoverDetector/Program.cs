01533115e206ce5cfd28351da7663a035465980b
Ze Chen <zeche@microsoft.com>
2017-07-19 10:59:34 -0700

    xml parser

--- FailoverDetector/Program.cs
+++ FailoverDetector/Program.cs
@@ -12,39 +12,58 @@ namespace FailoverDetector
     {
         static void Main(string[] args)
         {
-            //AlwaysOnData Node003 = new AlwaysOnData();
-            //Node003.loadData("C:\\Users\\zeche\\Documents\\WorkItems\\POC\\VM003_0.xel", "ZE-VM003");
-            //AlwaysOnData Node001 = new AlwaysOnData();
-            //Node001.loadData("C:\\Users\\zeche\\Documents\\WorkItems\\POC\\VM001_0.xel", "ZE-VM001");
+            AlwaysOnData Node003 = new AlwaysOnData();
+            Node003.loadData("C:\\Users\\zeche\\Documents\\WorkItems\\POC\\VM003_0.xel", "ZE-VM003");
+            AlwaysOnData Node001 = new AlwaysOnData();
+            Node001.loadData("C:\\Users\\zeche\\Documents\\WorkItems\\POC\\VM001_0.xel", "ZE-VM001");
 
-            //Node001.MergeInstance(Node003);
-            //Node001.AnalyzeReports();
-            //Node001.ShowFailoverReports();
-            //Console.ReadLine();
+            Node001.MergeInstance(Node003);
+            Node001.AnalyzeReports();
+            Node001.ShowFailoverReports();
             testDemo();
+            Console.ReadLine();
         }
-
         static void testDemo()
         {
-            TimeSpan diff = new TimeSpan(8, 0, 0);
-            DateTimeOffset start = new DateTimeOffset(2017, 05, 13, 20, 12, 00,diff);
-            DateTimeOffset end = new DateTimeOffset(2017, 05, 13, 20, 18, 00,diff);
-            using (QueryableXEventData evts = new QueryableXEventData("C:\\Users\\zeche\\Documents\\WorkItems\\POC\\SYS_000_0.xel"))
+            TimeSpan diff = new TimeSpan(-7, 0, 0);
+            DateTimeOffset start = new DateTimeOffset(2017, 07, 08, 22, 26, 00, diff);
+            DateTimeOffset end = new DateTimeOffset(2017, 07, 08, 22, 29, 00, diff);
+            string url = "C:\\Users\\zeche\\Documents\\WorkItems\\POC\\SYS003_0.xel";
+            using (QueryableXEventData evts = new QueryableXEventData(url))
             {
-                foreach(PublishedEvent evt in evts)
+                foreach (PublishedEvent evt in evts)
                 {
-                    if(evt.Timestamp > start && evt.Timestamp < end)
+
+                    if (evt.Name.ToString() == "sp_server_diagnostics_component_result")
                     {
-                        Console.WriteLine("Event: {0}, time:{1} ", evt.Name, evt.Timestamp);
-                        if(evt.Name.ToString() == "sp_server_diagnostics_component_result")
+                        String t_component = evt.Fields["component"].Value.ToString();
+                        String t_data = evt.Fields["data"].Value.ToString();
+                        SystemHealthParser parser = new SystemHealthParser();
+                        switch (t_component)
                         {
-                            Console.WriteLine("component : {0} \n, Data: {1}", evt.Fields["component"].Value.ToString(), evt.Fields["data"].Value.ToString());
+                            case "QUERY_PROCESSING":
+                                if (!parser.ParseQPComponent(t_data))
+                                {
+                                    Console.WriteLine("Event: {0}, time:{1} ", evt.Name, evt.Timestamp);
+                                }
+                                break;
+                            case "SYSTEM":
+                                parser.ParseSystemComponent(t_data);
+                                break;
+                            case "RESOURCE":
+                                parser.ParseResource(t_data);
+                                break;
+                            case "IO_SUBSYSTEM":
+                                parser.ParseIOSubsytem(t_data);
+                                break;
+                            default:
+                                break;
                         }
                     }
                 }
             }
-            Console.ReadLine();
         }
+
     }
 
 
