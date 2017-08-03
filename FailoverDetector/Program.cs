a793b617e53ce7f6268b6ad530fad7f691247645
Ze Chen <zeche@microsoft.com>
2017-08-03 14:11:43 -0700

    look up current directory and open xel file, remove hard code path

--- FailoverDetector/Program.cs
+++ FailoverDetector/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,13 +13,27 @@ namespace FailoverDetector
     {
         static void Main(string[] args)
         {
+            string path = Directory.GetCurrentDirectory();
+            path += @"\Data";
+            string[] nodes = Directory.GetDirectories(path);
+            List<AlwaysOnData> nodeList = new List<AlwaysOnData>();
+            AlwaysOnData instance = null;
+            int i = 0;
+            foreach( string node in nodes)
+            {
+                string xelPath = node + @"\*.xel";
+                string nodeName = node.Remove(0, path.Length + 1);
+                Console.WriteLine("xel file path is : {0}\n Node name: {1}", xelPath, nodeName);
+                instance = new AlwaysOnData();
+                instance.loadData(xelPath, nodeName);
+                if(i != 0 )
+                {
+                    nodeList.First().MergeInstance(instance);
+                }
+                nodeList.Add(instance);
 
-            AlwaysOnData Node003 = new AlwaysOnData();
-            Node003.loadData("C:\\Users\\zeche\\Documents\\WorkItems\\POC\\VM003_0.xel", "ZE-VM003");
-            AlwaysOnData Node001 = new AlwaysOnData();
-            Node001.loadData("C:\\Users\\zeche\\Documents\\WorkItems\\POC\\VM001_0.xel", "ZE-VM001");
-
-            Node001.MergeInstance(Node003);
+            }
+            AlwaysOnData Node001 = nodeList.First();
             Node001.AnalyzeReports();
             Node001.ShowFailoverReports();
 
