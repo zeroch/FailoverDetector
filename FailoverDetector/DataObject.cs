4115ddb581cf346b60e4b43d32cd122c661dd73c
Ze Chen <zeche@microsoft.com>
2017-09-26 14:43:01 -0700

    handle error preorted  in xevent

--- FailoverDetector/DataObject.cs
+++ FailoverDetector/DataObject.cs
@@ -133,7 +133,71 @@ namespace FailoverDetector
 
 
         public void HandleLockRedoBlocked(PublishedEvent evt) { }
-        public void HandleErrorReported(PublishedEvent evt) { }
+
+
+        // error code reported in this event. 
+
+        // --alwayson connection timeout information
+        // or[error_number]=(35201) --new connection timeout
+        // or[error_number]=(35202) --connected
+        //or[error_number] = (35206)--existing connection timeout
+        //or[error_number] = (35207)--general connection issue message
+
+        // check
+        // --wsfc cluster issues
+        // or[error_number]>(41047) and[error_number]<(41056)
+
+        // check
+        // --failover validation message
+        // or[error_number] = (41142)
+
+        // check
+        // --availability group resource failure
+        // or[error_number]=(41144) 
+
+        // check           
+        // --database replica role change
+        // or[error_number]=(1480) 
+
+        public void HandleErrorReported(PublishedEvent evt)
+        {
+            //read error code
+            string errCode = evt.Fields["error_number"].Value.ToString();
+
+            switch(errCode)
+            {
+                case "41144":
+                    break;
+                // ErrorFormat: The availability replica for availability group '%.*ls' on this instance of SQL Server cannot become the primary replica. One or more databases are not synchronized or have not joined the availability group. If the availability replica uses the asynchronous-commit mode, consider performing a forced manual failover (with possible data loss). Otherwise, once all local secondary databases are joined and synchronized, you can perform a planned manual failover to this secondary replica (without data loss). For more information, see SQL Server Books Online.
+                // ErrorCause: One or more databases in the local replica of the availability group are not synchronized or have not joined the Availability group.
+                // ErrorCorrectiveAction: If the availability replica uses the asynchronous-commit mode, consider performing a forced manual failover (with possible data loss). Otherwise, once all local secondary databases are joined and synchronized, you can perform a planned manual failover to this secondary replica (without data loss). For more information, see SQL Server Books Online.
+                case "41142":
+                    break;
+                // WSFC message
+                // ErrorFormat: Failed to obtain the Windows Server Failover Clustering (WSFC) node state for the local WSFC node (Error code %d).  If this is a WSFC availability group, the WSFC service may not be running or may not be accessible in its current state.  Otherwise, contact your primary support provider.  For information about this error code, see "System Error Codes" in the Windows Development documentation.
+                // ErrorCause: The WSFC service, if applicable, may not be running or may not be accessible in its current state.
+                case "41047":
+                    break;
+                // ErrorFormat: Always On Availability Groups: Local Windows Server Failover Clustering service has become unavailable. This is an informational message only. No user action is required.
+                // ErrorWinFSFormat: Always On Availability Groups: Local Windows Server Failover Clustering service has become unavailable. This is an informational message only. No user action is required.
+                case "41048":
+                    break;
+                // ErrorWinFSFormat: Always On Availability Groups: Local Windows Server Failover Clustering node is no longer online. This is an informational message only. No user action is required.
+                case "41049":
+                    break;
+                // ErrorFormat: Always On Availability Groups: Waiting for local Windows Server Failover Clustering node to come online. This is an informational message only. No user action is required.
+                // ErrorWinFSFormat: Always On Availability Groups: Waiting for local Windows Server Failover Clustering node to come online. This is an informational message only. No user action is required.
+                case "41054":
+                    break;
+                // ErrorFormat: Availability replica '%.*ls' of availability group '%.*ls' cannot be brought online on this SQL Server instance.  Another replica of the same availability group is already online on the node.  Each node can host only one replica of an availability group, regardless of the number of SQL Server instances on the node.  Use the ALTER AVAILABILITY GROUP command to correct the availability group configuration.  Then, if the other replica is no longer being hosted on this node, restart this instance of SQL Server to bring the local replica of the availability group online.
+                // ErrorCause: Another replica of the same availability group is already online on this node '%.*ls'.  Each node can host only one replica of an availability group, regardless of the number of SQL Server instances on the node.
+                // ErrorCorrectiveAction: Use the ALTER AVAILABILITY GROUP command to correct the availability group configuration.  Then, if the other replica is no longer being hosted on this node, restart this instance of SQL Server to bring the local replica of the availability group online.
+                case "41056":
+                    break;
+
+            }
+
+        }
         private void DispatchEvent(PublishedEvent evt)
         {
             switch(evt.Name)
