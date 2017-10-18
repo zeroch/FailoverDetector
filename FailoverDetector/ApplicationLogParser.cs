1b646788ee75e4f78e941bfcd8191a02c1e53e10
Ze Chen <zeche@microsoft.com>
2017-10-18 16:58:02 -0700

    Tokenizer for Application logs.

---
+++ FailoverDetector/ApplicationLogParser.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace FailoverDetector
+{
+    public class ApplicationLogParser : LogParser
+    {
+        public ApplicationLogParser()
+        {
+        }
+
+        private Regex rxTimeStamp = new Regex(@"\d{1,2}\/\d{1,2}\/\d{4}\s\d{2}:\d{2}:\d{2}\s+[AM|PM]{2}", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        private Regex rxEntryType = new Regex(@"Information|Error");
+        private Regex rxBrackets = new Regex(@"\[.*?\]");
+        private Regex rxMSSQLSource = new Regex(@"MSSQL\$\w+");
+
+        public override string TokenizeTimestamp(string line)
+        {
+            string tmp = string.Empty;
+            if( this.rxTimeStamp.IsMatch(line))
+            {
+                tmp = this.rxTimeStamp.Match(line).Value;
+            }
+            return tmp;
+        }
+        public string TokenizeEntryType(string line)
+        {
+            string tmp = string.Empty;
+            if (this.rxEntryType.IsMatch(line))
+            {
+                tmp = this.rxEntryType.Match(line).Value;
+            }
+            return tmp;
+        }
+
+        public string TokenizeSQLSource(string line)
+        {
+            string tmp = string.Empty;
+            if (this.rxMSSQLSource.IsMatch(line))
+            {
+                tmp = this.rxMSSQLSource.Match(line).Value;
+            }
+            return tmp;
+        }
+
+        public override ErrorLogEntry ParseLogEntry(string line)
+        {
+            string tmpTimestamp = TokenizeTimestamp(line);
+            line = line.Substring(tmpTimestamp.Length).Trim();
+
+            string tmpType = TokenizeEntryType(line);
+            line = line.Substring(tmpType.Length).Trim();
+
+            string tmpSQLSource = TokenizeSQLSource(line);
+            line = line.Substring(tmpSQLSource.Length ).Trim();
+
+            ErrorLogEntry entry = new ErrorLogEntry(tmpTimestamp, "", line);
+            return entry;
+
+        }
+
+
+    }
+}
---
+++ FailoverDetectorTests/ApplicationLogParserTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FailoverDetector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FailoverDetector.Tests
+{
+    [TestClass()]
+    public class ApplicationLogParserTests
+    {
+        string testString = @"9/19/2017 10:30:08 AM   Information MSSQL$SQL16RTM01       The state of the local availability replica in availability group 'ag8102017' has changed from   'RESOLVING_NORMAL' to 'PRIMARY_PENDING'.  The state changed because the availability group is coming online.  For more information, see the SQL Server error log, Windows Server Failover Clustering (WSFC) management console, or WSFC log.";
+        ApplicationLogParser appLogParser;
+        [TestInitialize]
+        public void Setup()
+        {
+            appLogParser = new ApplicationLogParser();
+
+        }
+
+        [TestMethod()]
+        public void TokenizeTimestampTest()
+        {
+            string retTime = appLogParser.TokenizeTimestamp(testString);
+            Console.WriteLine("Test Time stamp token: {0}", retTime);
+            Assert.AreEqual(retTime, "9/19/2017 10:30:08 AM");
+        }
+
+        [TestMethod()]
+        public void TokenizeEntryTypeTest()
+        {
+            string retType = appLogParser.TokenizeEntryType(testString);
+            Console.WriteLine("Test Message Type: {0}", retType);
+            Assert.AreEqual(retType, "Information");
+        }
+
+        [TestMethod()]
+        public void TokenizeSQLSourceTest()
+        {
+            string retSource = appLogParser.TokenizeSQLSource(testString);
+            Console.WriteLine("Test Application Log Source: {0}", retSource);
+            Assert.AreEqual(retSource, "MSSQL$SQL16RTM01");
+        }
+
+        [TestMethod()]
+        public void ParseLogEntryTest()
+        {
+            ErrorLogEntry ret = appLogParser.ParseLogEntry(testString);
+            ErrorLogEntry test = new ErrorLogEntry(@"9/19/2017 10:30:08 AM", @"", @"The state of the local availability replica in availability group 'ag8102017' has changed from   'RESOLVING_NORMAL' to 'PRIMARY_PENDING'.  The state changed because the availability group is coming online.  For more information, see the SQL Server error log, Windows Server Failover Clustering (WSFC) management console, or WSFC log.");
+            Assert.IsTrue(ret.Equals(test));
+        }
+    }
+}
